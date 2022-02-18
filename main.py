45b3d372fff7e0877eada0e325114380df7116a0
wangjingpeng <jingpeng.wang@ppiaas.cn>
2022-02-18 16:19:25 +0800

    finish first version

---
+++ main.py
@@ -0,0 +1,60 @@
+import threading
+
+import requests
+import re
+import time
+from bs4 import BeautifulSoup
+
+List = [
+    {'platform': 'bilibili', 'name': '双尾彗星', 'url': 'https://live.bilibili.com/1496449'},
+    {'platform': 'bilibili', 'name': 'test', 'url': 'https://live.bilibili.com/21495945'}
+]
+
+Platform_List = {
+    'bilibili': 180
+}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/71.0.3578.98 Safari/537.36'
+}
+
+
+def write(file_name, online_num):
+    file = open('data/' + file_name + '.txt', 'a')
+    data = str(time.time()) + ':' + str(online_num) + '\n'
+    file.write(data)
+
+
+def get_online_num_func(platform, response):
+    func = {
+        'bilibili': re.search(r'"online":(.+?),', response.text),
+    }
+    online = func.get(platform, "unknown platform")
+    if online:
+        return online.group(1)
+    else:
+        return -1
+
+
+def get_online_num(webcast_msg):
+    response = requests.get(webcast_msg['url'], headers=headers)
+    response.encoding = 'uft8'
+    write(webcast_msg['name'], get_online_num_func(webcast_msg['platform'], response))
+
+
+def limiter(webcast_msg, sleep_time):
+    while True:
+        get_online_num(webcast_msg)
+        time.sleep(sleep_time)
+
+
+def main(webcast_list, platform_list):
+    for i in webcast_list:
+        sleep_time = platform_list[i['platform']]
+        t = threading.Thread(target=limiter, args=(i, sleep_time))
+        t.start()
+
+
+if __name__ == '__main__':
+    main(List, Platform_List)
